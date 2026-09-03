273c721f83b49b36cafd0bc27e2099ef1d4bbcbd
Jakub Mareš <kubzej8@gmail.com>
2026-09-03 22:56:22 +0200

    feat(playwright): remove Playwright configuration file

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@playwright/test";
-
-export default defineConfig({
-  testDir: ".",
-  testMatch: /.*\.spec\.ts$/,
-  respectGitIgnore: false,
-  timeout: 30_000,
-  reporter: "list",
-});
